4870ea9429b1268f1c6e1415cebf7dfe3b67d910
Corentin VÉROT <corentin.verot@outlook.com>
2022-05-19 20:07:30 +0200

    Ajout crud + formulaire utilisateur

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +14,17 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-//            ->add('roles')
-            ->add('password')
-            ->add('pseudo')
+            ->add('pseudo', TextType::class)
+            ->add('email', TextType::class)
+            ->add('roles', ChoiceType::class, [
+                'multiple' => true,
+                'label' => 'Rôle',
+                'choices' => [
+                  "Standard" => 'ROLE_USER',
+                  "Administrateur" => 'ROLE_ADMIN',
+                ],
+            ])
+            ->add('password', TextType::class)
         ;
     }
 
