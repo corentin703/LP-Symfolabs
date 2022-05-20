9bc00e1ac4541c716a4ea2d427c2f4154d7144ba
Corentin VÉROT <corentin.verot@outlook.com>
2022-05-20 08:36:18 +0200

    Init pages promotions

---
+++ src/Form/PromotionType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Promotion;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PromotionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('discount')
+            ->add('created_at')
+            ->add('start_at')
+            ->add('expires_at')
+            ->add('became_hot_at')
+            ->add('delivery_fees')
+            ->add('company')
+            ->add('author')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Promotion::class,
+        ]);
+    }
+}
