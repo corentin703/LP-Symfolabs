53ec8d3529828c67d9dee83ebd1c314faa8a2b6b
Corentin VÉROT <corentin.verot@outlook.com>
2022-06-03 16:51:23 +0200

    Ajout API promos

---
+++ src/Controller/Api/ApiPromotionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Controller\PromotionController;
+use App\Repository\PromotionRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiPromotionController extends AbstractController
+{
+    /**
+     * @Route("/api/promotion", name="api_promotion", methods={"GET"})
+     */
+    public function index(PromotionRepository $promotionRepository): Response
+    {
+        return $this->json($promotionRepository->findAll());
+    }
+}
