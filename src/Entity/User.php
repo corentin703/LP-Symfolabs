7a54f4689c1c76760d5ead2b579f4942fbe61ac1
Corentin VÉROT <corentin.verot@outlook.com>
2022-06-17 09:28:12 +0200

    Ajout page de profil

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -37,7 +37,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @ORM\Column(type="json")
      */
-    private array $roles = [];
+    private array $roles = ['ROLE_USER'];
 
     /**
      * @var string The hashed password
@@ -125,8 +125,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
